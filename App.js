5dc1c809008cc10773bbcd0ca5356ffca4eb2565
Caner Al <caneral5858@gmail.com>
2022-05-12 14:24:19 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, StatusBar, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <StatusBar backgroundColor={'white'}/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
