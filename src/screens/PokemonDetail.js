39c765f6fb0b47253345b2191794fbce8ea2a698
Caner Al <caneral5858@gmail.com>
2022-05-14 12:43:25 +0300

    Back Icon added.

--- src/screens/PokemonDetail.js
+++ src/screens/PokemonDetail.js
@@ -1,32 +1,66 @@
-import {StyleSheet, Text, View, Image, FlatList} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
 import React, {useEffect} from 'react';
 import {POKEMON_FRONT_DEFAULT_PATH} from '../configs/api';
 import {useDispatch, useSelector} from 'react-redux';
 import {getPokemon} from '../redux/actions/pokemon';
 import BaseStats from '../components/BaseStats';
+import Icon from 'react-native-vector-icons/AntDesign';
 
-const PokemonDetail = ({route}) => {
+const PokemonDetail = ({route, navigation}) => {
   const {color, pokemonId, pokemonName} = route.params;
   const dispatch = useDispatch();
   const store = useSelector(state => state.pokemons);
   const pokemon = store?.pokemonData;
-  const pokemonStats = pokemon.stats;
+  const pokemonStats = pokemon?.stats;
+  const pokemonTypes = pokemon?.types;
+
   useEffect(() => {
     dispatch(getPokemon(pokemonId));
   }, []);
+
   return (
     <View style={styles.container(color)}>
       <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon name="arrowleft" size={30} color="white" />
+        </TouchableOpacity>
         <View
           style={{
             flexDirection: 'row',
-            height: 100,
+            height: 70,
             alignItems: 'center',
             justifyContent: 'space-between',
             width: '100%',
           }}>
           <Text style={styles.title}>{pokemonName}</Text>
           <Text style={styles.subtitle}>#{pokemonId}</Text>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <FlatList
+            horizontal={true}
+            showsVerticalScrollIndicator={false}
+            data={pokemonTypes}
+            renderItem={({item}) => (
+              <View style={styles.badge}>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    fontWeight: '600',
+                    color: 'black',
+                    textAlign: 'center',
+                  }}>
+                  {item.type.name.toUpperCase()}
+                </Text>
+              </View>
+            )}
+          />
         </View>
       </View>
       <View style={styles.footer}>
@@ -59,9 +93,8 @@ const styles = StyleSheet.create({
   }),
   header: {
     flex: 1,
-    paddingTop: 50,
+    paddingTop: 30,
     padding: 20,
-    flexDirection: 'row',
   },
   title: {
     fontSize: 32,
@@ -95,5 +128,13 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 30,
     marginTop: 80,
+  },
+  badge: {
+    backgroundColor: 'white',
+    color: 'black',
+    padding: 12,
+    borderRadius: 32,
+    width: 80,
+    marginHorizontal: 5,
   },
 });
